518ff713fe056e457dafb60fa63ea61f38e42f2e
Steven Malis <smmalis37@gmail.com>
2020-06-18 00:28:53 -0700

    Implement part 3.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ use itertools::Itertools;
 use std::fs::{read_to_string, write};
 
 fn main() -> Result<()> {
-    for i in 0..=2 {
+    for i in 0..=3 {
         let input = read(i)?;
         let output = match i {
-            0 => part0(&input).collect(),
+            0 => part0(&input),
             1 => part1(&input),
             2 => part2(&input),
+            3 => part3(&input),
             _ => unreachable!(),
         };
         write(get_path(i + 1), output)?;
@@ -62,16 +63,22 @@ fn decode_ascii85_chunk(x: &[u8]) -> impl Iterator<Item = u8> {
     std::array::IntoIter::new(value.to_be_bytes()).take(4 - padding_count)
 }
 
-fn part0(input: &[u8]) -> impl Iterator<Item = u8> + '_ {
+fn decode_ascii85(input: &[u8]) -> impl Iterator<Item = u8> + '_ {
     input.chunks(5).flat_map(decode_ascii85_chunk)
 }
 
+fn part0(input: &[u8]) -> Vec<u8> {
+    decode_ascii85(input).collect()
+}
+
 fn part1(input: &[u8]) -> Vec<u8> {
-    part0(input).map(|x| (x ^ 0x55).rotate_right(1)).collect()
+    decode_ascii85(input)
+        .map(|x| (x ^ 0x55).rotate_right(1))
+        .collect()
 }
 
 fn part2(input: &[u8]) -> Vec<u8> {
-    part0(input)
+    decode_ascii85(input)
         .filter(|x| x.count_ones() % 2 == 0)
         .chunks(8)
         .into_iter()
@@ -84,4 +91,34 @@ fn part2(input: &[u8]) -> Vec<u8> {
             std::array::IntoIter::new(value.to_be_bytes()).take(7)
         })
         .collect()
+}
+
+fn part3(input: &[u8]) -> Vec<u8> {
+    const KEY_SIZE: usize = 32;
+    const KNOWN_PLAINTEXT: &[u8] = b"==[ Payload ]===================";
+    assert!(KNOWN_PLAINTEXT.len() == KEY_SIZE);
+    const VERIFY_TEXT: &[u8] = b"==[ Payload ]===============================================";
+
+    let cipher_text: Vec<_> = part0(input);
+
+    for (i, w) in cipher_text.windows(KEY_SIZE).enumerate() {
+        let mut possible_key: Vec<_> = w
+            .iter()
+            .zip(KNOWN_PLAINTEXT)
+            .map(|(x1, x2)| x1 ^ x2)
+            .collect();
+        possible_key.rotate_right(i % KEY_SIZE);
+
+        let decoded_text: Vec<_> = cipher_text
+            .iter()
+            .zip(possible_key.iter().cycle())
+            .map(|(x1, x2)| x1 ^ x2)
+            .collect();
+
+        if decoded_text[i..].starts_with(VERIFY_TEXT) {
+            return decoded_text;
+        }
+    }
+
+    unreachable!()
 }
